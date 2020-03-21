e41d89be97a543f307fe1fdddd818636d9369663
Denys <chornijj14@gmail.com>
2020-03-21 23:37:32 +0200

    Formed a card with the conclusion of the cheapest tickets

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const  formSearch = document.querySelector('.form-search'),
         dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
         inputCitiesTo = document.querySelector('.input__cities-to'),
         dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
-        inputDateDepart = document.querySelector('.input__date-depart');
+        inputDateDepart = document.querySelector('.input__date-depart'),
+        cheapestTicket = document.getElementById('cheapest-ticket'),
+        otherCheapTickets = document.getElementById('other-cheap-tickets');
 
 let city = [];
 
@@ -66,8 +68,48 @@ const hideCity = (event, input, list) =>{ //Hide the dropdown list after choosen
     }
 };
 
+const createCard = (data) =>{
+    const ticket = document.createElement('article');
+    ticket.classList.add('ticket');
+
+    let deep = '';
+    if(data){
+        deep = `
+        <h3 class="agent">Aviakassa</h3>
+            <div class="ticket__wrapper">
+                <div class="left-side">
+                    <a href="https://www.aviasales.ru/search/SVX2905KGD1" class="button button__buy">Купить
+                        за 19700₽</a>
+                </div>
+                <div class="right-side">
+                    <div class="block-left">
+                        <div class="city__from">Вылет из города
+                            <span class="city__name">Екатеринбург</span>
+                        </div>
+                        <div class="date">29 мая 2020 г.</div>
+                    </div>
+            
+                    <div class="block-right">
+                        <div class="changes">Без пересадок</div>
+                        <div class="city__to">Город назначения:
+                            <span class="city__name">Калининград</span>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        `;
+    }else{
+        deep = '<h3> Unfortunately for the current date, there are no tickets </h3>';
+    }
+
+    ticket.insertAdjacentHTML('beforeend', deep);
+
+    return ticket;
+};
+
 const renderCheapDay = (cheapTicket) => {
-    console.log(cheapTicket);
+    const ticket = createCard(cheapTicket[0]);
+    console.log(ticket);
 };
 
 const renderCheapYear = (cheapTickets) => {
